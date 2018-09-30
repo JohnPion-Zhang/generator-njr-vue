c269e884a5eda3d95bfd2cec47fceaac635ac464
倪俊杰 <1006312908@qq.com>
2018-09-30 14:38:26 +0800

    components直接引入

--- generators/app/templates/src/@selections/vuex/index.js
+++ generators/app/templates/src/@selections/vuex/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill' // https://babeljs.io/docs/plugins/preset-env/#usebuiltins
 import 'common/scss/index'
 import Vue from 'vue'
+import './componentsBase'
+import './components'
 import router from './router'
 import App from './app'
 import './plugins'
